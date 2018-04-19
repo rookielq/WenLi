3751a701d18da9a3e4627304c0bf854fc5241b70
lvhaosri <985694388@qq.com>
2018-04-19 23:56:35 +0800

    学生登录功能的测试，以及友好返回提示信息。

--- src/main/java/cn/lvhaosir/controller/StudentsController.java
+++ src/main/java/cn/lvhaosir/controller/StudentsController.java
@@ -1,10 +1,40 @@
 package cn.lvhaosir.controller;
 
+import cn.lvhaosir.entity.Students;
+import cn.lvhaosir.entity.vo.StudentsVo;
+import cn.lvhaosir.service.StudentsService;
+import cn.lvhaosir.utils.EmptyUtil;
+import cn.lvhaosir.utils.JsonReturnData;
+import cn.lvhaosir.utils.WebConstant;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 /**
  * Created by lvhaosir on 2018/4/19.
  */
 @Controller
+@RequestMapping("/students")
 public class StudentsController {
+
+    @Autowired
+    private StudentsService studentsService;
+
+    @RequestMapping(value = "/login" )
+    @ResponseBody
+    public JsonReturnData login(Students student) {
+        if(EmptyUtil.isEmpty(student.getStudentCardId()) || EmptyUtil.isEmpty(student.getStudentPassword())) {
+            return new JsonReturnData(WebConstant.VALID_DATA,"无有效数据");
+        }else {
+            StudentsVo login = studentsService.login(student);
+            if(EmptyUtil.isEmpty(login)) {
+                return new JsonReturnData(WebConstant.LOGIN_ERROR,"账号或密码错误");
+            }
+            return new JsonReturnData<StudentsVo>("登录成功",login);
+        }
+    }
+
+
+
 }
--- src/main/java/cn/lvhaosir/utils/WebConstant.java
+++ src/main/java/cn/lvhaosir/utils/WebConstant.java
@@ -14,7 +14,15 @@ public class WebConstant {
      *  出现错误
      */
     public static final Integer ERROR=500;
+    /**
+     *  没有接收到有效数据
+     */
+    public static final Integer VALID_DATA=300;
 
+    /**
+     *  登录账号密码错误
+     */
+    public static final Integer LOGIN_ERROR=201;
 
 
 }
